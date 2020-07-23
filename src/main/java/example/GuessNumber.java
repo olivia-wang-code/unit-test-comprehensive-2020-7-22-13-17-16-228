6da4f6badf3cd3d1ab6c5292c219db2db93ad979
olivia <wangol@oocl.com>
2020-07-23 21:12:37 +0800

    [Guess Number TDD][olivia]feature pass should_return_4AOB_when_guess_give_answer_1234_and_input_guess_1234

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -6,7 +6,7 @@ public class GuessNumber {
    public String guess(int[] inputGuess,int[] answer) {
        boolean isAllCorrect=true;
        if (Arrays.equals(inputGuess, answer)){
-           return "aa";
+           return "4A0B";
        }
        return null;
    }
