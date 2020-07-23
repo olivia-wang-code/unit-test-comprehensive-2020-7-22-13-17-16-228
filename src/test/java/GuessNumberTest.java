cdd22efa2274bd3aa54f703fd51fd4fe91278d09
olivia <wangol@oocl.com>
2020-07-23 23:29:17 +0800

    [Guess Number TDD][olivia]test  should_return_0A4B_when_guess_give_answer_1234_and_input_guess_4321

--- src/test/java/GuessNumberTest.java
+++ src/test/java/GuessNumberTest.java
@@ -31,5 +31,17 @@ public class GuessNumberTest {
         //then
         assertEquals("2A2B", result);
         }
+    @Test
+    void should_return_0A4B_when_guess_give_answer_1234_and_input_guess_4321() {
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        //»¨À¨ºÅ
+        List<Integer> answer = Arrays.asList(1, 2, 3, 4);
+        List<Integer> inputGuess = Arrays.asList(4,3,2,1);
+        //when
+        String result = guessNumber.guess(inputGuess, answer);
+        //then
+        assertEquals("0A4B", result);
+    }
 }
 
