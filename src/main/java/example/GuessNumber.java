c73219a383be81e80999461ac464ca6bdee0a244
olivia-wang-code <2350625418@qq.com>
2020-07-24 18:53:44 +0800

    [Guess Number TDD][olivia]refactor:refactor functions which count the value of A and B

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -5,85 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GuessNumber {
-   public String guess(List inputGuess,List answer) {
-       if (isAllCorrect(inputGuess, answer)) return "4A0B";
-       if(isOutput2A2B(inputGuess,answer)) return "2A2B";
-       if(isOutput0A4B(inputGuess,answer)) return "0A4B";
-       if(isOutput0A0B(inputGuess,answer)) return "0A0B";
-       if(isOutput1A0B(inputGuess,answer)) return "1A0B";
-       return null;
-   }
-
-    private boolean isAllCorrect(List inputGuess, List answer) {
+    public String guess(List<Integer> inputGuess, List<Integer> answer) {
         boolean isAllCorrect = true;
-        int count=0;
+        int countA = 0;
+        int countB = 0;
         for (int index = 0; index < 4; index++) {
-            if (inputGuess.get(index) == answer.get(index)) {
-                count++;
+            if (inputGuess.get(index).equals(answer.get(index))) {
+                countA++;
+                continue;
+            }
+            if (inputGuess.contains(answer.get(index))) {
+                countB++;
             }
 
         }
-        return count==4;
-    }
-    private boolean isOutput2A2B(List inputGuess, List answer) {
-        boolean isAllCorrect=true;
-        int countA=0;
-        int countB=0;
-       for(int index=0;index<4;index++){
-           if(inputGuess.get(index)==answer.get(index)){
-               countA++;
-           }
-       }
-
-        if(isEqualsList(inputGuess,answer)){
-           countB=4-countA;
-       }
-        return countA==2&&countB==2;
-    }
-    private boolean isEqualsList(List inputGuess,List answer){
-       for(int i=0;i<4;i++){
-           if(!inputGuess.contains(answer.get(i))){
-               return false;
-           };
-       }
-      return true;
-    }
-    private boolean isOutput0A4B(List inputGuess, List answer) {
-        boolean isAllCorrect = true;
-        int count=0;
-        for (int index = 0; index < 4; index++) {
-            if (inputGuess.get(index) != answer.get(index)) {
-                count++;
-            }
-        }
-        return count==4&&isEqualsList(inputGuess,answer);
-    }
-    private boolean isOutput0A0B(List inputGuess, List answer) {
-       int count=0;
-        for(int i=0;i<4;i++){
-            if(!inputGuess.contains(answer.get(i))){
-                count++;
-            }
-        }
-        return count==4;
-    }
-    private boolean isOutput1A0B(List inputGuess, List answer) {
-       int count=0;
-        List newList1=new ArrayList();
-        List newList2=new ArrayList();
-        newList1.addAll(inputGuess);
-        newList2.addAll(answer);
-        for(int i=0;i<4;i++){
-            if(inputGuess.get(i)==answer.get(i)){
-                newList1.remove(i);
-                newList2.remove(i);
-            }
-        }
-        for(int i=0;i<3;i++){
-            if(!newList1.contains(newList2.get(i))){
-                count++;
-            }
-        }
-        return count==3;
+        return String.format("%sA%sB", countA, countB);
     }
 }
+
--- src/test/java/GuessNumberTest.java
+++ src/test/java/GuessNumberTest.java
@@ -11,7 +11,6 @@ public class GuessNumberTest {
     void should_return_4AOB_when_guess_give_answer_1234_and_input_guess_1234() {
         //given
         GuessNumber guessNumber = new GuessNumber();
-        //»¨À¨ºÅ
         List<Integer> answer = Arrays.asList(1, 2, 3, 4);
         List<Integer> inputGuess = Arrays.asList(1, 2, 3, 4);
         //when
@@ -20,34 +19,9 @@ public class GuessNumberTest {
         assertEquals("4A0B", result);
     }
     @Test
-    void should_return_2A2B_when_guess_give_answer_1234_and_input_guess_1324() {
-        //given
-        GuessNumber guessNumber = new GuessNumber();
-        //»¨À¨ºÅ
-        List<Integer> answer = Arrays.asList(1, 2, 3, 4);
-        List<Integer> inputGuess = Arrays.asList(1, 3, 2, 4);
-        //when
-        String result = guessNumber.guess(inputGuess, answer);
-        //then
-        assertEquals("2A2B", result);
-        }
-    @Test
-    void should_return_0A4B_when_guess_give_answer_1234_and_input_guess_4321() {
-        //given
-        GuessNumber guessNumber = new GuessNumber();
-        //»¨À¨ºÅ
-        List<Integer> answer = Arrays.asList(1, 2, 3, 4);
-        List<Integer> inputGuess = Arrays.asList(4,3,2,1);
-        //when
-        String result = guessNumber.guess(inputGuess, answer);
-        //then
-        assertEquals("0A4B", result);
-    }
-    @Test
     void should_return_0A0B_when_guess_give_answer_1234_and_input_guess_5678() {
         //given
         GuessNumber guessNumber = new GuessNumber();
-        //»¨À¨ºÅ
         List<Integer> answer = Arrays.asList(1, 2, 3, 4);
         List<Integer> inputGuess = Arrays.asList(5,6,7,8);
         //when
@@ -59,7 +33,6 @@ public class GuessNumberTest {
     void should_return_1A0B_when_guess_give_answer_1234_and_input_guess_1756() {
         //given
         GuessNumber guessNumber = new GuessNumber();
-        //»¨À¨ºÅ
         List<Integer> answer = Arrays.asList(1, 2, 3, 4);
         List<Integer> inputGuess = Arrays.asList(1,7,5,6);
         //when
@@ -68,10 +41,20 @@ public class GuessNumberTest {
         assertEquals("1A0B", result);
     }
     @Test
+    void should_return_2A2B_when_guess_give_answer_1234_and_input_guess_1324() {
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        List<Integer> answer = Arrays.asList(1, 2, 3, 4);
+        List<Integer> inputGuess = Arrays.asList(1, 3, 2, 4);
+        //when
+        String result = guessNumber.guess(inputGuess, answer);
+        //then
+        assertEquals("2A2B", result);
+        }
+    @Test
     void should_return_0A2B_when_guess_give_answer_1234_and_input_guess_3456() {
         //given
         GuessNumber guessNumber = new GuessNumber();
-        //»¨À¨ºÅ
         List<Integer> answer = Arrays.asList(1, 2, 3, 4);
         List<Integer> inputGuess = Arrays.asList(3,4,5,6);
         //when
@@ -79,5 +62,17 @@ public class GuessNumberTest {
         //then
         assertEquals("0A2B", result);
     }
+    @Test
+    void should_return_0A4B_when_guess_give_answer_1234_and_input_guess_4321() {
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        List<Integer> answer = Arrays.asList(1, 2, 3, 4);
+        List<Integer> inputGuess = Arrays.asList(4,3,2,1);
+        //when
+        String result = guessNumber.guess(inputGuess, answer);
+        //then
+        assertEquals("0A4B", result);
+    }
+
 }
 
