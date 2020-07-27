ddcf5aef05d731c0c0c911d6a36c75c540ddc21a
olivia <wangol@oocl.com>
2020-07-27 22:00:01 +0800

    [Guess Number][olivia]feature: pass should_return_false_when_guess_given_input_guess_has_same_number

--- src/main/java/example/AvailableInput.java
+++ src/main/java/example/AvailableInput.java
@@ -3,6 +3,13 @@ import java.util.List;
 
 public class AvailableInput {
    public Boolean isValid(List<Integer> guess) {
+      int temp=guess.get(0);
+      for (int i = 1; i < 4; i++) {
+         if (temp == guess.get(i)) {
+            return false;
+         }
+         temp = guess.get(i);
+      }
       return true;
    }
 }
