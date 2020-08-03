4b239b58a804f160ec3a14c2268638dba131ce0d
olivia-wang-code <2350625418@qq.com>
2020-08-03 10:31:59 +0800

    [Guess Number][olivia]bug-fix: fix function isValid(List<Integer> guess)

--- src/main/java/example/AvailableInput.java
+++ src/main/java/example/AvailableInput.java
@@ -8,9 +8,9 @@ public class AvailableInput {
         if (guess.size() != validGuessLength) {
             return false;
         }
-        for (int i = 1; i < 4; i++) {
-            for (int j = 1; j < 4; j++)
-                if (guess.get(i).intValue()==guess.get(j).intValue()) return false;
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 4; j++)
+                if (guess.get(i).intValue() == guess.get(j).intValue()) return false;
         }
         return true;
     }
